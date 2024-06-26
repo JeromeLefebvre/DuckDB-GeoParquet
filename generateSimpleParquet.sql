eaab9fd65c02bf75bb191508c5b574f127f80ffb
Jerome Lefebvre <jerome.p.lefebvre@gmail.com>
2024-06-27 04:39:03 +0800

    Initial Generation of Parquet

---
+++ generateSimpleParquet.sql
@@ -0,0 +1,9 @@
+
+-- Create a simple geoparquet file
+copy (
+    from range(1,876576)
+    select
+        ST_AsWKB(st_point(0,0)) as geometry,
+        date'1900-01-01 00:00:00' + interval (range) hours as time,
+        range % 10 as OneToTen 
+) to 'Maps/oneCenturyCompressed.parquet' (format 'parquet', COMPRESSION 'zstd', ROW_GROUP_SIZE 160);
